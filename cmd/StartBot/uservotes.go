2aa21b7e318893a6c4a99378c82836eaa43d6a9b
Isuru Weerasooriya <Isuru Weerasooriya>
2017-05-02 22:04:05 -0400

    Added uservote struct

---
+++ cmd/StartBot/uservotes.go
@@ -0,0 +1,18 @@
+package kick_user
+
+type uservotes struct {
+	time int
+	votes []string
+}
+
+func userVotes yesGreateThanNo() bool {
+
+}
+
+func userVotes getYesVoteCount() int {
+
+} 
+
+func userVotes getNoVoteCount() int {
+	
+}
